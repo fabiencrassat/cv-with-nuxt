35d89b3d807dfea8c13ee7ad194432d2f913747f
Fabien Crassat <fabien@crassat.com>
2020-05-02 19:32:47 +0200

    Enhance navigation side bar

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,15 +1,36 @@
 <template>
   <nav class="fixed top-0 left-0 w-24 h-full bg-gray-500 flex h-screen">
     <ul class="m-auto">
-      <li>
+      <li class="p-4">
         <nuxt-link
           :to="localePath(this.homepage)"
           class="flex flex-col items-center"
         >
-          <UserIcon :title="$t('about')" />
-          <span class="uppercase">
-            {{ $t('about') }}
-          </span>
+          <AboutIcon :title="$t('about')" />
+        </nuxt-link>
+      </li>
+      <li class="p-4">
+        <nuxt-link
+          :to="localePath(this.homepage)"
+          class="flex flex-col items-center"
+        >
+          <ContactIcon :title="$t('contact')" />
+        </nuxt-link>
+      </li>
+      <li class="p-4">
+        <nuxt-link
+          :to="localePath(this.homepage)"
+          class="flex flex-col items-center"
+        >
+          <JobIcon :title="$t('jobs')" />
+        </nuxt-link>
+      </li>
+      <li class="p-4">
+        <nuxt-link
+          :to="localePath(this.homepage)"
+          class="flex flex-col items-center"
+        >
+          <SkillIcon :title="$t('skills')" />
         </nuxt-link>
       </li>
     </ul>
@@ -17,11 +38,17 @@
 </template>
 
 <script>
-import UserIcon from './icons/user';
+import AboutIcon from './icons/user';
+import ContactIcon from './icons/phone';
+import SkillIcon from './icons/glasses';
+import JobIcon from './icons/suitcase';
 
 export default {
   components: {
-    UserIcon,
+    AboutIcon,
+    ContactIcon,
+    SkillIcon,
+    JobIcon,
   },
   props: {
     homepage: {
@@ -35,10 +62,16 @@ export default {
 <i18n>
 {
   "en": {
-    "about": "About"
+    "about": "About",
+    "contact": "Contact Me",
+    "jobs": "My Jobs",
+    "skills": "My Skills"
   },
   "fr": {
-    "about": "A propos"
+    "about": "A propos",
+    "contact": "Contactez-moi",
+    "jobs": "Mes expériences",
+    "skills": "Mes compétences"
   }
 }
 </i18n>
