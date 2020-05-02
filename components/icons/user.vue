de54d1f9fcf299149eb34f25e1802cb004f8ae2a
Fabien Crassat <fabien@crassat.com>
2020-05-02 18:20:08 +0200

    Add user icon

---
+++ components/icons/user.vue
@@ -0,0 +1,39 @@
+<template>
+  <svg
+    class="h-12 w-12 fill-current"
+    viewBox="0 0 32 32"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M16,16A7,7,0,1,0,9,9,7,7,0,0,0,16,16ZM16,4a5,5,0,1,1-5,5A5,5,0,0,1,16,4Z"
+    />
+    <path
+      d="M17,18H15A11,11,0,0,0,4,29a1,1,0,0,0,1,1H27a1,1,0,0,0,1-1A11,11,0,0,0,17,18ZM6.06,28A9,9,0,0,1,15,20h2a9,9,0,0,1,8.94,8Z"
+    />
+    <title>{{ this.title }}</title>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: function () {
+        return this.$t('title');
+      },
+    },
+  },
+};
+</script>
+
+<i18n>
+{
+  "en": {
+    "title": "User"
+  },
+  "fr": {
+    "title": "Utilisateur"
+  }
+}
+</i18n>
