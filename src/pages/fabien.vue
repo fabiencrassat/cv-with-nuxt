3a11b767bf91884faa80c7d1b5947c6fd8c073fa
Fabien Crassat <fabien@crassat.com>
2020-05-19 12:56:39 +0200

    Fix blank link to prevent the original page from being modified by the opened link

--- src/pages/fabien.vue
+++ src/pages/fabien.vue
@@ -38,7 +38,13 @@
               d="M10,2.009c-2.762,0-5,2.229-5,4.99c0,4.774,5,11,5,11s5-6.227,5-11C15,4.239,12.762,2.009,10,2.009z M10,9.76c-1.492,0-2.7-1.209-2.7-2.7s1.208-2.7,2.7-2.7c1.49,0,2.699,1.209,2.699,2.7S11.49,9.76,10,9.76z"
             />
           </svg>
-          <a target="_blank" href="http://goo.gl/maps/UJrFf">Paris, France</a>
+          <a
+            href="http://goo.gl/maps/UJrFf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Paris, France
+          </a>
         </p>
       </section>
       <section id="my-jobs">
