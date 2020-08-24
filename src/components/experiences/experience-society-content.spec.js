e98f7c5b5d6b91883a9c0c7805fb4b1fafe5da95
Fabien Crassat <fabien@crassat.com>
2020-08-24 23:28:07 +0200

    Better society images

--- src/components/experiences/experience-society-content.spec.js
+++ src/components/experiences/experience-society-content.spec.js
@@ -20,7 +20,9 @@ describe('content', () => {
         },
       })
     ).toMatchInlineSnapshot(`
-      <div class="flex items-center"><img src="/img/societies/office-building.svg" class="w-10 h-10 rounded-full mr-4">
+      <div class="flex items-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-10 h-10 mr-4">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
+        </svg>
         <div class="text-sm">
           <p class="leading-none">
 
@@ -45,7 +47,7 @@ describe('content', () => {
         },
       })
     ).toMatchInlineSnapshot(`
-      <div class="flex items-center"><img src="path/to/icon" alt="society name" class="w-10 h-10 rounded-full mr-4">
+      <div class="flex items-center"><img src="path/to/icon" alt="society name" title="society name" class="w-10 h-10 mr-4">
         <div class="text-sm">
           <p class="leading-none">
             society name
--- src/components/experiences/experience-society-content.vue
+++ src/components/experiences/experience-society-content.vue
@@ -2,10 +2,27 @@
   <div class="flex items-center">
     <!-- eslint-disable-next-line vue/html-self-closing -->
     <img
-      class="w-10 h-10 rounded-full mr-4"
-      :src="society.icon ? society.icon : '/img/societies/office-building.svg'"
+      v-if="society.icon"
+      class="w-10 h-10 mr-4"
+      :src="society.icon"
       :alt="society.name"
+      :title="society.name"
     />
+    <svg
+      v-else
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      class="w-10 h-10 mr-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
+      />
+    </svg>
     <div class="text-sm">
       <p class="leading-none">
         {{ society.name }}
