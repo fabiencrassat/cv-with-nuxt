f535030e693e8d48472293f31a77decd6276e0e8
Fabien Crassat <fabien.crassat@reacteev.com>
2024-08-29 18:57:08 +0200

    Use vercel-edge for nitro in vercel pipeline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,6 +57,9 @@ export default defineNuxtConfig({
     vueI18n: './config/i18n.config.ts',
   },
   modules: ['@nuxtjs/i18n', '@nuxtjs/tailwindcss', '@nuxt/test-utils/module', '@nuxt/eslint', 'nuxt-gtag'],
+  nitro: {
+    preset: 'vercel-edge',
+  },
   routeRules: {
     '/': { redirect: '/fabien' },
     '/fabien/en': { redirect: '/en/fabien' },
