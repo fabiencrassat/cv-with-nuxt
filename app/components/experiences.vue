8dcb3971667ed854706ddaa314d4c01333897c07
Fabien Crassat <fabien.crassat@gmail.com>
2026-07-25 11:54:37 +0200

    Add an explicit "type" attribute to the expand experiences button

--- app/components/experiences.vue
+++ app/components/experiences.vue
@@ -14,6 +14,7 @@
     >
       <button
         class="bg-white text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
+        type="submit"
         @click="showMore"
       >
         {{ $t('showMore') }}
